f92f73d25df1342f35c68b647c4d450eec73a2a2
defend3 <150699643+defend3@users.noreply.github.com>
2025-05-08 11:08:49 +1000

    Update dy.js

--- assets/kit/dy.js
+++ assets/kit/dy.js
@@ -180,123 +180,6 @@ $(document).ready(function () {
       // for tablet and mobile
     }
   
-    // click effect
-    var anicolors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C'];
-    function clickEffect() {
-  
-      var canvasEl = document.querySelector('.fireworks');
-      var ctx = canvasEl.getContext('2d');
-      var numberOfParticules = 30;
-      var pointerX = 0;
-      var pointerY = 0;
-      var tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
-  
-  
-      function setCanvasSize() {
-        canvasEl.width = window.innerWidth * 2;
-        canvasEl.height = window.innerHeight * 2;
-        canvasEl.style.width = window.innerWidth + 'px';
-        canvasEl.style.height = window.innerHeight + 'px';
-        canvasEl.getContext('2d').scale(2, 2);
-      }
-  
-      function updateCoords(e) {
-        pointerX = e.clientX || e.touches[0].clientX;
-        pointerY = e.clientY || e.touches[0].clientY;
-      }
-  
-      function setParticuleDirection(p) {
-        var angle = anime.random(0, 360) * Math.PI / 180;
-        var value = anime.random(50, 180);
-        var radius = [-1, 1][anime.random(0, 1)] * value;
-        return {
-          x: p.x + radius * Math.cos(angle),
-          y: p.y + radius * Math.sin(angle)
-        }
-      }
-  
-      function createParticule(x, y) {
-        var p = {};
-        p.x = x;
-        p.y = y;
-        p.color = anicolors[anime.random(0, anicolors.length - 1)];
-        p.radius = anime.random(16, 32);
-        p.endPos = setParticuleDirection(p);
-        p.draw = function () {
-          ctx.beginPath();
-          ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-          ctx.fillStyle = p.color;
-          ctx.fill();
-        }
-        return p;
-      }
-  
-      function createCircle(x, y) {
-        var p = {};
-        p.x = x;
-        p.y = y;
-        p.color = '#FFF';
-        p.radius = 0.1;
-        p.alpha = .5;
-        p.lineWidth = 6;
-        p.draw = function () {
-          ctx.globalAlpha = p.alpha;
-          ctx.beginPath();
-          ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-          ctx.lineWidth = p.lineWidth;
-          ctx.strokeStyle = p.color;
-          ctx.stroke();
-          ctx.globalAlpha = 1;
-        }
-        return p;
-      }
-  
-      function renderParticule(anim) {
-        for (var i = 0; i < anim.animatables.length; i++) {
-          anim.animatables[i].target.draw();
-        }
-      }
-  
-      function animateParticules(x, y) {
-        var circle = createCircle(x, y);
-        var particules = [];
-        for (var i = 0; i < numberOfParticules; i++) {
-          particules.push(createParticule(x, y));
-        }
-        anime.timeline().add({
-          targets: particules,
-          x: function (p) { return p.endPos.x; },
-          y: function (p) { return p.endPos.y; },
-          radius: 0.1,
-          duration: anime.random(1200, 1800),
-          easing: 'easeOutExpo',
-          update: renderParticule
-        })
-      }
-  
-      var render = anime({
-        duration: Infinity,
-        update: function () {
-          ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-        }
-      });
-  
-      document.addEventListener(tap, function (e) {
-        window.human = true;
-        render.play();
-        updateCoords(e);
-        animateParticules(pointerX, pointerY);
-      }, false);
-  
-      var centerX = window.innerWidth / 2;
-      var centerY = window.innerHeight / 2;
-  
-  
-      setCanvasSize();
-      window.addEventListener('resize', setCanvasSize, false);
-  
-    } clickEffect();
-  
     // tab message
     $(window).blur(function () {
       document.title = "where do you think you're going?";
@@ -346,7 +229,6 @@ $(document).ready(function () {
         $('.dyPlayer').attr('src', 'assets/media/dev.mp3');
         $('.dyPlayer')[0].play();
         $('.dyProfile>img').attr('src', 'assets/img/hacker.png');
-        anicolors = ["#00ff00", "#000"];
       }
     });
     function runMatrix() {
